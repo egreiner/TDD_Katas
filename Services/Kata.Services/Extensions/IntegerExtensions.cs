eba99d346279e936bfce920fa37d05ca19f35eeb
Ernst <ernst.greiner@ieg-tools.net>
2020-11-29 16:42:18 +0100

    extend IntegerExtensions

--- Services/Kata.Services/Extensions/IntegerExtensions.cs
+++ Services/Kata.Services/Extensions/IntegerExtensions.cs
@@ -2,10 +2,16 @@
 {
     public static class IntegerExtensions
     {
-        // TODO not tested
         public static bool IsBetween(this int value, int min, int max)
         {
             return value >= min && value <= max;
+        }
+
+        public static int LimitTo(this int value, int min, int max)
+        {
+            if (value < min) return min;
+            if (value > max) return max;
+            return value;
         }
 
         public static bool IsEven(this int value) => value % 2 == 0;
