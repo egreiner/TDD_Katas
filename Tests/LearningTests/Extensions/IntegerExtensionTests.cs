9d3f374254f5f37e4edfb8e47abb27c82daaf5dc
Ernst <ernst.greiner@ieg-tools.net>
2020-11-30 10:38:54 +0100

    improve ExtensionMethods, add tests

--- Tests/LearningTests/Extensions/IntegerExtensionTests.cs
+++ Tests/LearningTests/Extensions/IntegerExtensionTests.cs
@@ -5,33 +5,6 @@
 
     public class IntegerExtensionTests
     {
-        [Theory]
-        [InlineData(1, 100, 1, true)]
-        [InlineData(1, 100, 2, true)]
-        [InlineData(1, 100, 100, true)]
-        [InlineData(1, 100, -2, false)]
-        public void Test_IsBetween(int min, int max, int value, bool expected)
-        {
-            var actual = value.IsBetween(min, max);
-
-            Assert.Equal(expected, actual);
-        }
-
-        [Theory]
-        [InlineData(1, 100, 1, 1)]
-        [InlineData(1, 100, 2, 2)]
-        [InlineData(1, 100, 100, 100)]
-        [InlineData(1, 100, -2, 1)]
-        [InlineData(1, 100, 200, 100)]
-        public void Test_LimitTo(int min, int max, int value, int expected)
-        {
-            var actual = value.LimitTo(min, max);
-
-            Assert.Equal(expected, actual);
-        }
-
-
-
         // DO NOT SKIP SUCH A SIMPLE TEST!!!
         [Theory]
         [InlineData(1, false)]
