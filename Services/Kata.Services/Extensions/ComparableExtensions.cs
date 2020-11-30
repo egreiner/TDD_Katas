1d19e6ad023d0879fcb260f1fc9e9c8b9439dfc5
Ernst <ernst.greiner@ieg-tools.net>
2020-11-30 10:38:54 +0100

    improve ExtensionMethods, add tests

---
+++ Services/Kata.Services/Extensions/ComparableExtensions.cs
@@ -0,0 +1,20 @@
+﻿namespace Kata.Services.Extensions
+{
+    using System;
+
+    public static class ComparableExtensions
+    {
+        public static bool IsBetween<T>(this T value, T min, T max) where T : IComparable<T> =>
+            value.CompareTo(min) >= 0 && value.CompareTo(max) <= 0;
+
+
+        public static T LimitTo<T>(this T value, T min, T max) where T : IComparable<T> =>
+            value.LimitToMin(min).LimitToMax(max);
+
+        public static T LimitToMin<T>(this T value, T min) where T : IComparable<T> =>
+            value.CompareTo(min) > 0 ? value : min;
+
+        public static T LimitToMax<T>(this T value, T max) where T : IComparable<T> =>
+            value.CompareTo(max) > 0 ? max : value;
+    }
+}
--- Services/Kata.Services/Extensions/IntegerExtensions.cs
+++ Services/Kata.Services/Extensions/IntegerExtensions.cs
@@ -2,18 +2,6 @@
 {
     public static class IntegerExtensions
     {
-        public static bool IsBetween(this int value, int min, int max)
-        {
-            return value >= min && value <= max;
-        }
-
-        public static int LimitTo(this int value, int min, int max)
-        {
-            if (value < min) return min;
-            if (value > max) return max;
-            return value;
-        }
-
         public static bool IsEven(this int value) => value % 2 == 0;
 
         public static bool IsOdd(this int value) => value % 2 != 0;
