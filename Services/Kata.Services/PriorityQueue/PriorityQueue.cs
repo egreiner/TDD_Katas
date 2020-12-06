e66f44da2b3353414db9a671075dedaece5ff8ec
Ernst <ernst.greiner@ieg-tools.net>
2020-12-06 10:16:38 +0100

    add PriorityQueue

---
+++ Services/Kata.Services/PriorityQueue/PriorityQueue.cs
@@ -0,0 +1,53 @@
+﻿namespace Kata.Services.PriorityQueue
+{
+    using System;
+    using System.Collections.Concurrent;
+    using System.Linq;
+
+    public class PriorityQueue<T>
+    {
+        private readonly ConcurrentBag<PriorityItem<T>> queue = 
+            new ConcurrentBag<PriorityItem<T>>();
+
+
+        public int Count => this.queue.Count;
+
+        public bool IsEmpty => this.queue.IsEmpty;
+        
+        
+        public void Enqueue(T item, int priority) =>
+            this.queue.Add(new PriorityItem<T>(item, priority));
+
+        public bool TryDequeue(out T element)
+        {
+            element = default;
+            var first = this.queue.Where(x => x != null)
+                                 .OrderBy(x => x.Priority)
+                                 .ThenBy(x => x.Created).FirstOrDefault();
+
+            if (first == null) return false;
+
+            element = first.Item;
+            this.queue.TryTake(out first);
+
+            return true;
+        }
+
+
+        private class PriorityItem<TItem>
+        {
+            public PriorityItem(TItem item, int priority)
+            {
+                this.Created  = DateTime.Now;
+                this.Item     = item;
+                this.Priority = priority;
+            }
+
+            public TItem Item { get; }
+            
+            public int Priority { get; }
+
+            public DateTime Created { get; }
+        }
+    }
+}
---
+++ Tests/UnitTests.Services/PriorityQueue/PriorityQueueTests.cs
@@ -0,0 +1,83 @@
+﻿namespace UnitTests.Services.PriorityQueue
+{
+    using Kata.Services.PriorityQueue;
+    using Xunit;
+
+
+    public class PriorityQueueTests
+    {
+        [Fact]
+        public void Test_IsEmpty_true()
+        {
+            var cut = new PriorityQueue<int>();
+
+            var actual = cut.IsEmpty;
+
+            Assert.True(actual);
+        }
+
+
+        [Fact]
+        public void Test_IsEmpty_false()
+        {
+            var cut = new PriorityQueue<int>();
+            cut.Enqueue(2, 1);
+
+            var actual = cut.IsEmpty;
+
+            Assert.False(actual);
+        }
+
+        [Fact]
+        public void Test_Count()
+        {
+            var cut = new PriorityQueue<int>();
+
+            cut.Enqueue(2, 1);
+            cut.Enqueue(2, 1);
+
+            var actual = cut.Count;
+
+            Assert.Equal(2, actual);
+        }
+
+        [Fact]
+        public void Test_TryDequeue_from_empty_queue()
+        {
+            var cut = new PriorityQueue<int>();
+
+            var actual = cut.TryDequeue(out var item);
+
+            Assert.False(actual);
+            Assert.Equal(0, item);
+        }
+
+        [Fact]
+        public void Test_TryDequeue_first_added()
+        {
+            var cut = new PriorityQueue<int>();
+
+            cut.Enqueue(2, 1);
+            cut.Enqueue(3, 1);
+
+            var actual = cut.TryDequeue(out var item);
+
+            Assert.True(actual);
+            Assert.Equal(2, item);
+        }
+
+        [Fact]
+        public void Test_TryDequeue_second_added()
+        {
+            var cut = new PriorityQueue<int>();
+
+            cut.Enqueue(2, 2);
+            cut.Enqueue(3, 1);
+
+            var actual = cut.TryDequeue(out var item);
+
+            Assert.True(actual);
+            Assert.Equal(3, item);
+        }
+    }
+}
