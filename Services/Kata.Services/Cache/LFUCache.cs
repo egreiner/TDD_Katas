ccb2680554301439fe73994b4bb12611d7b0873e
Ernst <ernst.greiner@ieg-tools.net>
2020-12-14 15:24:43 +0100

    add LFUCache and tests

---
+++ Services/Kata.Services/Cache/LFUCache.cs
@@ -0,0 +1,35 @@
+﻿namespace Kata.Services.Cache
+{
+    using System.Linq;
+
+    /// <summary>
+    /// Least Frequently Used cache (LFU)
+    /// </summary>
+    public class LFUCache<TKey, TValue>: Cache<TKey, TValue>
+    {
+        public LFUCache(int maxCacheLength)
+        {
+            this.MaxCacheLength = maxCacheLength;
+        }
+
+        public int MaxCacheLength { get; }
+
+
+        public override void Set(TKey key, TValue value)
+        {
+            this.RemoveToLength(this.MaxCacheLength - 1);
+            base.Set(key, value);
+        }
+
+
+        private void RemoveToLength(int maxLength)
+        {
+            while (this.CachedItems.Count > maxLength)
+            {
+                var x = this.CachedItems.OrderBy(x => x.Value.FetchCount)
+                    .FirstOrDefault();
+                this.CachedItems.TryRemove(x);
+            }
+        }
+    }
+}
--- Tests/UnitTests.Services/Cache/LFUCacheTests.cs
+++ Tests/UnitTests.Services/Cache/LFUCacheTests.cs
@@ -3,7 +3,7 @@
     using Kata.Services.Cache;
     using Xunit;
 
-    public class LRUCacheTests
+    public class LFUCacheTests
     {
         [Fact]
         public void Test_CacheLength_does_not_exceed_maximum()
@@ -21,30 +21,7 @@
         }
 
         [Theory]
-        [InlineData(1, false)]
-        [InlineData(2, true)]
-        [InlineData(10, true)]
-        [InlineData(11, true)]
-        [InlineData(12, false)]
-        public void Test_First_added_item_get_removed(int value, bool expected)
-        {
-            var maxLength = 10;
-            var cut = GetCache(maxLength);
-
-            for (int i = 1; i <= 11; i++)
-                cut.Set(i, i);
-
-            var actual = cut.Contains(value);
-
-            Assert.Equal(expected, actual);
-        }
-
-        [Theory]
         [InlineData(1, true)]
-        [InlineData(2, false)]
-        [InlineData(3, true)]
-        [InlineData(10, true)]
-        [InlineData(11, true)]
         [InlineData(12, false)]
         public void Test_First_added_item_get_not_removed(int value, bool expected)
         {
@@ -63,7 +40,7 @@
         }
 
 
-        private static LRUCache<int, int> GetCache(int maxLength) =>
-            new LRUCache<int, int>(maxLength);
+        private static LFUCache<int, int> GetCache(int maxLength) =>
+            new LFUCache<int, int>(maxLength);
     }
 }
